c8c0b4630d223e31b05e0921450ce6b2f44b482e
Darahaas Yajamanyam <darahaas15@gmail.com>
2022-06-09 01:48:18 +0530

    added claim condition

--- scripts/3-config-nft.js
+++ scripts/3-config-nft.js
@@ -1,13 +1,15 @@
 import sdk from './1-initialize-sdk.js';
 import { readFileSync } from 'fs';
 
-const editionDrop = sdk.getEditionDrop('INSERT_EDITION_DROP_ADDRESS');
+const editionDrop = sdk.getEditionDrop(
+    '0x3537EBF724919c47dD76f1412B747C7e8E16020C'
+);
 
 (async () => {
     try {
         await editionDrop.createBatch([
             {
-                name: 'Leaf Village Headband',
+                name: 'DAOrahaas Logo',
                 description: 'This NFT will give you access to DAOrahaas!',
                 image: readFileSync('scripts/assets/shipinabottle.png')
             }
