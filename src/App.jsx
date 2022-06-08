0d71fabd3ee1075bb3c6a14b85e617dc44e606e5
Darahaas Yajamanyam <darahaas15@gmail.com>
2022-06-09 04:58:31 +0530

    deploy token

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,27 @@ const App = () => {
     );
   }
 
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to DAOrahaas</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+  
+  // Add this little piece!
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>DAOrahaas Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  };
+
   // Render mint nft screen.
   return (
     <div className="mint-nft">
